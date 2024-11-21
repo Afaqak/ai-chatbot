1a25ad829b2812ce1f29c09b6850411775667a21
afaq <afaqak124@gmail.com>
2024-11-21 11:21:22 +0500

    vercel

--- app/(auth)/auth.config.ts
+++ app/(auth)/auth.config.ts
@@ -2,8 +2,8 @@ import type { NextAuthConfig } from 'next-auth';
 
 export const authConfig = {
   pages: {
-    signIn: '/login',
-    newUser: '/',
+    // signIn: '/login',
+    // newUser: '/',
   },
   providers: [
     // added later in auth.ts since it requires bcrypt which is only compatible with Node.js
@@ -16,22 +16,22 @@ export const authConfig = {
       const isOnRegister = nextUrl.pathname.startsWith('/register');
       const isOnLogin = nextUrl.pathname.startsWith('/login');
 
-      if (isLoggedIn && (isOnLogin || isOnRegister)) {
-        return Response.redirect(new URL('/', nextUrl as unknown as URL));
-      }
+      // if (isLoggedIn && (isOnLogin || isOnRegister)) {
+      //   return Response.redirect(new URL('/', nextUrl as unknown as URL));
+      // }
 
-      if (isOnRegister || isOnLogin) {
-        return true; // Always allow access to register and login pages
-      }
+      // if (isOnRegister || isOnLogin) {
+      //   return true; // Always allow access to register and login pages
+      // }
 
-      if (isOnChat) {
-        if (isLoggedIn) return true;
-        return false; // Redirect unauthenticated users to login page
-      }
+      // if (isOnChat) {
+      //   if (isLoggedIn) return true;
+      //   return false; // Redirect unauthenticated users to login page
+      // }
 
-      if (isLoggedIn) {
-        return Response.redirect(new URL('/', nextUrl as unknown as URL));
-      }
+      // if (isLoggedIn) {
+      //   return Response.redirect(new URL('/', nextUrl as unknown as URL));
+      // }
 
       return true;
     },
